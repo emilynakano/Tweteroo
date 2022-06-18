c1ad31519a0a9d031efe3692721134683abc64a9
emily <nakanodepaula@gmail.com>
2022-06-18 15:25:49 -0300

    post tweet

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const server = express();
 server.use(express.json());
 server.use(cors())
 const pessoas = [];
+const tweets = [];
 
 server.post('/sign-up', (req, res) => {
     const pessoa = req.body;
     pessoas.push(pessoa);
+    res.send("OK");
+})
+server.post('/tweets', (req, res) => {
+    const tweet = req.body;
+    tweets.push(tweet);
     res.send("OK");
 })
 
